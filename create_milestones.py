925b0ddaf255528c1ba56baaa1975abb8c805414
Eric Berquist <ejberqu@sandia.gov>
2024-11-15 09:49:54 -0700

    pass repo as CLI argument

--- create_milestones.py
+++ create_milestones.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
+import argparse
 import subprocess as sp
 from pathlib import Path
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--repo", default="berquist/test-create-milestones")
+    args = parser.parse_args()
+
     ret = sp.run(["gh", "extension", "install", "valeriobelli/gh-milestone"])
 
     __dir__ = Path(__file__).parent
     milestone_names = (__dir__ / "milestones.txt").read_text(encoding="utf-8").splitlines()
 
-    REPO = "berquist/test-create-milestones"
-
     for milestone_name in milestone_names:
         ret = sp.run(
-            ["gh", "milestone", "create", "--title", milestone_name, "--repo", REPO],
+            ["gh", "milestone", "create", "--title", milestone_name, "--repo", args.repo],
         )
